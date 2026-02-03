7401b25e4926ee897812fb8d3e925af2d1d8de53
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-04 00:42:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-window.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vg00G9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTLK9-xBpwx tmuVAQkA/exec";
+// config.js
+window.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vg00G9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTlK9-xBpwxmtuVAQkA/exec";
+
+// 統一用 window.CONFIG
+window.CONFIG = window.CONFIG || {};
+window.CONFIG.GAS_ENDPOINT = window.GAS_ENDPOINT;
