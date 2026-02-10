eb335d39824df04094c4858a957c08b1d9247b67
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-10 20:51:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // config.js
-window.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vgOOG9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTlK9-xBpwxtmuVAQkA/exec";
-
-// 統一用 window.CONFIG
 window.CONFIG = window.CONFIG || {};
-window.CONFIG.GAS_ENDPOINT = window.GAS_ENDPOINT;
+window.CONFIG.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vgOOG9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTlK9-xBpwxtmuVAQkA/exec";
+
+window.CONFIG.WEBHOOK_KEY = "HOLDON2026";
