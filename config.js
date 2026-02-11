3785366da8d415d925152d07f2d51183c03d348a
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-11 17:45:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
+// config.js
 window.CONFIG = window.CONFIG || {};
-
-// 請確認這是你最新的 GAS 部署網址 (Exec URL)
 window.CONFIG.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vgOOG9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTlK9-xBpwxtmuVAQkA/exec";
-
-// 你的 Webhook Key
 window.CONFIG.WEBHOOK_KEY = "HOLDON2026";
